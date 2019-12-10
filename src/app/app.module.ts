d968b2e3301b66244a370a5ca630d91ee1a204f2
SURYA <suryamss09@gmail.com>
2019-12-10 22:03:30 +0530

    Component Creation manualy / using ng g c command

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector : 'app-success-alert',
+    styleUrls: ['./success-alert.component.css'],
+    templateUrl : './success-alert.component.html'
+
+})
+export class SuccessAlertComponent implements OnInit{
+
+    constructor(){}
+
+    ngOnInit(){
+
+    }
+
+}
