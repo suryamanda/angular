f821a2af982a64fa254f1c6af869f58f1e6b86db
SURYA <suryamss09@gmail.com>
2020-01-14 17:21:35 +0530

    usage of @Input for sending data from outside of the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  serverElements = [{type: 'server', name:'Test Server', content:'Just a Test!'}];
 
 
+  
 }
--- src/app/shared/ingredient.model.ts
+++
@@ -1,4 +0,0 @@
-export class Ingredient{
-    //we can use access specifiers for constructor arguments
-      constructor(public name:string, public amount:number){}
-}
