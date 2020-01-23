fc291167b7c8525a5560cfca2a7f9aaa944995e9
SURYA <suryamss09@gmail.com>
2020-01-24 01:12:15 +0530

    Story : Using @HostBinding to Bind to Host Properties Details : with @HostBinding we can directly change DOM element properties without           the help of renderer2 as well as elementRef

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -7,20 +7,25 @@ export class BetterHighlightDirective implements OnInit {
 //we should always use Renderer to make changes to the DOM, as in some
 //cases we will not have access to DOM.
 
-  constructor(private elRef: ElementRef, private renderer : Renderer2) { }
+@HostBinding('style.backgroundColor') backgroundColor : string = 'transparent';
+/* 
+with the help of @HostBinding we dnt need to use Renderer and ElementRef, 
+we can directly select the DOM element and apply the changes
+*/
+  constructor(/* private elRef: ElementRef, private renderer : Renderer2 */) { }
 
   ngOnInit(){
     //this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue', false, false);
   }
 
-  
 @HostListener('mouseenter') mouseOver(eventData : Event){
-  this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
+  //this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
+  this.backgroundColor = 'blue';
 }
 
-
 @HostListener('mouseleave') mouseLeave(eventData : Event){
-  this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'transparent');
+  //this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'transparent');
+  this.backgroundColor = 'transparent';
 }
 
 }
