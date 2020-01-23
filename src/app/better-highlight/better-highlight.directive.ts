9978cdb9f2112f19d7ea0ac21c7b06575a6d5ff9
SURYA <suryamss09@gmail.com>
2020-01-24 01:30:19 +0530

    Story : Binding to Directive Properties Details : we can pass directive properties by implementing @Input inside custom Property Directive.

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,13 +1,14 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-//we should always use Renderer to make changes to the DOM, as in some
-//cases we will not have access to DOM.
 
-@HostBinding('style.backgroundColor') backgroundColor : string = 'transparent';
+  @Input() defaultColor : string = 'transparent';
+  @Input() highlightColor : string = 'blue';
+
+@HostBinding('style.backgroundColor') backgroundColor : string = this.defaultColor;
 /* 
 with the help of @HostBinding we dnt need to use Renderer and ElementRef, 
 we can directly select the DOM element and apply the changes
@@ -15,17 +16,18 @@ we can directly select the DOM element and apply the changes
   constructor(/* private elRef: ElementRef, private renderer : Renderer2 */) { }
 
   ngOnInit(){
+    this.backgroundColor= this.defaultColor;
     //this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue', false, false);
   }
 
 @HostListener('mouseenter') mouseOver(eventData : Event){
   //this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
-  this.backgroundColor = 'blue';
+  this.backgroundColor = this.highlightColor;
 }
 
 @HostListener('mouseleave') mouseLeave(eventData : Event){
   //this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'transparent');
-  this.backgroundColor = 'transparent';
+  this.backgroundColor = this.defaultColor;
 }
 
 }
