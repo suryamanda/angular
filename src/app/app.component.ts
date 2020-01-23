2fd6731f7e3611247bd24c58ab2950e399f19c69
SURYA <suryamss09@gmail.com>
2020-01-23 22:50:43 +0530

    Adding navigation with the help of @Output event emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  selectedFeature: string ='recipe';
+
+  onSelectedFeature(feature: string){
+    this.selectedFeature = feature;
+  }
 
 }
