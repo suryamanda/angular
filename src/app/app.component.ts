bce9dcb19024f85309a3d80769f06f8f30b86a60
SURYA <suryamss09@gmail.com>
2020-02-26 20:17:50 +0530

    angular app without routing enabled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
-  display: boolean = false;
-  clicks : any = [];
-  toggleDisplay(){
-    this.display= !this.display;
-    this.clicks.push(new Date());
-    
-  }
- 
-
 }
