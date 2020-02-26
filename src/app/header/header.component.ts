177ca3a967f4b2b40de84eadfdae7b792f0960e7
SURYA <suryamss09@gmail.com>
2020-02-27 01:12:51 +0530

    Adding routing to recipe book

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,15 +7,9 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  @Output() featureSelected = new EventEmitter<string>();
-
   constructor() { }
 
   ngOnInit() {
-  }
-
-  onSelectFeature(feature: string){
-    this.featureSelected.emit(feature);
   }
 
 }
